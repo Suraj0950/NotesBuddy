7ff6754f7ada115695598f1b8f0fc20469d6b660
Suraj0950 <surajsuryaveer@gmail.com>
2025-11-22 16:53:48 +0530

    project setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <h1>Hello World!..</h1>
+      <h1 className='flex items-center justify-center font-extrabold text-5xl font-mono text-red-600 bg-amber-500 h-80px w-full'>Hello World...</h1> 
     </>
   )
 }
